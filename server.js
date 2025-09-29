70a963130ab6945ab1a2cbbb454cd869a287801e
CIISE CAALIM AHMED <ciisecaalim63@gmail.com>
2025-09-28 23:40:47 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,45 +1,31 @@
-const express = require("express");
-const mongoose = require("mongoose");
-require("dotenv").config();
+const express = require("express")
+const mongoose = require("mongoose")
+require("dotenv").config()
+const producRouter = require("./router/productRouter")
+const customerRouter = require("./router/customerRouter")
+const orderRouter = require("./router/orderRoutes")
+const userRouter = require("./router/useRouter")
+const adminRouter = require("./router/adminRouter")
+const cors = require('cors');
 
-const producRouter = require("./router/productRouter");
-const customerRouter = require("./router/customerRouter");
-const orderRouter = require("./router/orderRoutes");
-const userRouter = require("./router/useRouter");
-const adminRouter = require("./router/adminRouter");
-const cors = require("cors");
+const app = express()
 
-const app = express();
+app.use(cors())
 
-// Middlewares
-app.use(cors());
-app.use(express.json());
+const port = process.env.port || 5000
+app.use(express.json())
+ 
 
-// MongoDB connection
-mongoose
-  .connect(process.env.MONGODB_URL)
-  .then(() => {
-    console.log("MongoDB connected...");
-  })
-  .catch((err) => console.error("MongoDB connection error:", err));
+mongoose.connect(process.env.mongodb_url).then(() => {
+      console.log("connected..")
+})
 
-// Test route
-app.get("/", (req, res) => {
-  res.send("Backend API is running...");
-});
 
-// Routers
-app.use(producRouter);
-app.use(customerRouter);
-app.use(orderRouter);
-app.use(userRouter);
-app.use(adminRouter);
+app.use(producRouter)
+app.use(customerRouter)
+app.use(orderRouter)
+app.use(userRouter)
+app.use(adminRouter)
+app.use("/allImg", express.static("document")); // Ensure the directory name is correct
 
-// Static files
-app.use("/allImg", express.static("document"));
-
-// Port (Render uses process.env.PORT)
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(port, () => { console.log(`server is running on port ${port}`)})
