b8aab3c8dff9388d0c8f5174d7864f95625244c1
CIISE CAALIM AHMED <ciisecaalim63@gmail.com>
2025-09-28 23:19:17 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,38 +1,45 @@
-const express = require("express")
-const mongoose = require("mongoose")
-require("dotenv").config()
-const producRouter = require("./router/productRouter")
-const customerRouter = require("./router/customerRouter")
-const orderRouter = require("./router/orderRoutes")
-const userRouter = require("./router/useRouter")
-const adminRouter = require("./router/adminRouter")
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-const app = express()
+const producRouter = require("./router/productRouter");
+const customerRouter = require("./router/customerRouter");
+const orderRouter = require("./router/orderRoutes");
+const userRouter = require("./router/useRouter");
+const adminRouter = require("./router/adminRouter");
+const cors = require("cors");
 
-app.use(cors())
+const app = express();
 
-// const port = process.env.port || 5000
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// Middlewares
+app.use(cors());
+app.use(express.json());
 
-app.use(express.json())
- 
+// MongoDB connection
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("MongoDB connected...");
+  })
+  .catch((err) => console.error("MongoDB connection error:", err));
 
-mongoose.connect(process.env.mongodb_url).then(() => {
-      console.log("connected..")
-})
-
-
+// Test route
 app.get("/", (req, res) => {
   res.send("Backend API is running...");
 });
 
-app.use(producRouter)
-app.use(customerRouter)
-app.use(orderRouter)
-app.use(userRouter)
-app.use(adminRouter)
-app.use("/allImg", express.static("document")); // Ensure the directory name is correct
+// Routers
+app.use(producRouter);
+app.use(customerRouter);
+app.use(orderRouter);
+app.use(userRouter);
+app.use(adminRouter);
 
-app.listen(port, () => { console.log(`server is running on port ${port}`)})
+// Static files
+app.use("/allImg", express.static("document"));
+
+// Port (Render uses process.env.PORT)
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
