5215a1e581de3128804b278311e334004062e563
Ciise Caalim Ahmed <walalkapo@gmail.com>
2025-10-04 09:34:50 -0700

    up

--- server.js
+++ server.js
@@ -1,31 +1,29 @@
+require("dotenv").config()
 const express = require("express")
 const mongoose = require("mongoose")
-require("dotenv").config()
-const producRouter = require("./router/productRouter")
+const cors = require("cors")
+
+const productRouter = require("./router/productRouter")
 const customerRouter = require("./router/customerRouter")
 const orderRouter = require("./router/orderRoutes")
 const userRouter = require("./router/useRouter")
 const adminRouter = require("./router/adminRouter")
-const cors = require('cors');
 
 const app = express()
-
 app.use(cors())
-
-const port = process.env.PORT || 5000
 app.use(express.json())
- 
 
-mongoose.connect(process.env.MONGODB_URL).then(() => {
-      console.log("connected..")
-})
+const port = process.env.PORT
 
+mongoose.connect(process.env.MONGODB_URL)
+  .then(() => console.log("MongoDB connected.."))
+  .catch(err => console.error("MongoDB connection error:", err))
 
-app.use(producRouter)
+app.use(productRouter)
 app.use(customerRouter)
 app.use(orderRouter)
 app.use(userRouter)
 app.use(adminRouter)
-app.use("/allImg", express.static("document")); // Ensure the directory name is correct
+app.use("/allImg", express.static("document"))
 
-app.listen(port, () => { console.log(`server is running on port ${port}`)})
+app.listen(port, () => console.log(`Server running on port ${port}`))
